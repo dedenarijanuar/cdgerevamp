04248d6bfebcbd63a6467a5b6654455cb56af8f7
Deden <dedenarijanuar@gmail.com>
2021-09-15 14:58:10 +0700

    add lottie player

--- src/templates/joinus-page.js
+++ src/templates/joinus-page.js
@@ -3,6 +3,7 @@ import "../styles/bootstrap/bootstrap.min.css"
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 export const JoinPageTemplate = ({
     title,
@@ -11,6 +12,14 @@ export const JoinPageTemplate = ({
       <div>
           <h1>{title}</h1>
           <p>{description}</p>
+          <Player
+          autoplay
+          loop
+          src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
+          style={{ height: '300px', width: '300px' }}
+        >
+          <Controls visible={true} buttons={['play', 'repeat', 'frame', 'debug']} />
+        </Player>
       </div>
   )
 
